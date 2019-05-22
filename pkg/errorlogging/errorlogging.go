746a9bcca8128ae90fdb38225ef69abe191f467b
jeffvswanson <jeffvswanson@gmail.com>
2019-05-22 11:09:01 -0600

    Added TODO for planning purposes.

--- pkg/errorlogging/errorlogging.go
+++ pkg/errorlogging/errorlogging.go
@@ -13,3 +13,16 @@ func ErrorCheck(message string, err error) {
 		log.Println(message, err)
 	}
 }
+
+/*
+TODO: Implement logging such that the errors are written to an output file.
+
+The output file goes into a directory:
+	logs/
+		YYYY/
+			MM-Month/
+				YYYYMMMDD.log
+
+Format of log entry:
+	Prefix = YYYY-MM-DD HH:MM:SS,ms UTC - package name - ErrorLevel - message
+*/
