8ac91f365dcb7a8514f389d4f070f7f1cf30c2ea
jeffvswanson <jeff.v.swanson@gmail.com>
2019-05-23 14:13:42 -0600

    Change instances of t.Logf to t.Errorf
    t.Logf was neither providing log information nor printing to stderr.
    This was causing the tests to pass instead of printing an error message.
    Switching to t.Errorf now causes tests to fail appropriately.

--- main_test.go
+++ main_test.go
@@ -10,7 +10,7 @@ func TestCsvSetup(t *testing.T) {
 	expected := "CSV setup complete."
 	got := csvSetup("TestCSV")
 	if got != expected {
-		t.Logf("csvSetup file name error. Expected: %v, Got: %v", expected, got)
+		t.Errorf("csvSetup file name error. Expected: %v, Got: %v", expected, got)
 	}
 	f, _ := os.Open("TestCSV.csv")
 	defer os.Remove("TestCSV.csv")
@@ -21,7 +21,7 @@ func TestCsvSetup(t *testing.T) {
 	expected = "URL,top_color1,top_color2,top_color3"
 	got = scanner.Text()
 	if got != expected {
-		t.Logf("csvSetup header error.\nExpected: %v\n Got:\t%v\n", expected, got)
+		t.Errorf("csvSetup header error.\nExpected: %v\n Got:\t%v\n", expected, got)
 	}
 }
 
@@ -37,12 +37,12 @@ func TestExtractURLs(t *testing.T) {
 	for _, url := range expected {
 		_, ok := got[url]
 		if !ok {
-			t.Logf("URL not present in imgColorPrevalence map: %v", url)
+			t.Errorf("URL not present in imgColorPrevalence map: %v", url)
 		}
 	}
 	expectedLength := 1000
 	gotLength := len(got)
 	if gotLength != expectedLength {
-		t.Logf("Not all URLs extracted. Expected: %d URLs, Got %d", expectedLength, gotLength)
+		t.Errorf("Error extracting URLs. Expected: %d URLs, Got %d", expectedLength, gotLength)
 	}
 }
