9dacd4d6e1cdbe138712d90b2f36f6c8b490e46f
jeffvswanson <jeff.v.swanson@gmail.com>
2019-06-22 20:38:19 -0600

    Remove csv setup test as tested in exporttocsv

--- main_test.go
+++ main_test.go
@@ -9,28 +9,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-/*
-func TestCsvSetup(t *testing.T) {
-	expected := "CSV setup complete."
-	got := csvSetup("TestCSV")
-	if got != expected {
-		t.Errorf("csvSetup file name error. Expected: %v, Got: %v", expected, got)
-	}
-
-	f, _ := os.Open("TestCSV.csv")
-	defer os.Remove("TestCSV.csv")
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	scanner.Scan()
-	expected = "URL,top_color1,top_color2,top_color3"
-	got = scanner.Text()
-	if got != expected {
-		t.Errorf("csvSetup header error.\nExpected: %v\n Got:\t%v\n", expected, got)
-	}
-}
-*/
-
 func TestExtractURLs(t *testing.T) {
 	expected := "Process complete."
 	got := extractURLs("input_test.txt", csvfile)
